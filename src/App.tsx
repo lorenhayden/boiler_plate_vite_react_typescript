72cf0c4ce71af98157c2b3c71b7c6f5770ee153a
Loren Hayden <lorenhayden@hotmail.com>
2024-01-05 11:38:48 -0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import tsLogo from './assets/ts.svg'
+import sassLogo  from './assets/sass.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.typescriptlang.org/" target="_blank">
+          <img src={tsLogo} className="logo" alt="Typescript logo" />
+        </a>
+        <a href="https://sass-lang.com/" target="_blank">
+          <img src={sassLogo} className="logo" alt="Sass logo" />
+        </a>
+      </div>
+      <h1>Vite + React + Typescript + Sass</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
